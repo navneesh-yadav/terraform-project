eab844878d2ee054262d39e27982ffbb903db562
Ubuntu <ubuntu@ip-172-31-24-24.ec2.internal>
2026-05-06 17:46:21 +0000

    modified /environment/dev/backend.tf for correct dytable name

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -3,7 +3,8 @@ terraform {
     bucket         = "navneesh-yadav-terraform-project-tf-state-bucket"    # Paste output here
     key            = "dev/terraform.tfstate" 
     region         = "us-east-1"
-    dynamodb_table = "navneesh-yadav-terraform-project-tf-state-bucket"    # Paste output here
+    dynamodb_table = "navneesh-yadav-terraform-project-tf-locks"    # Paste output here
     encrypt        = true
   }
 }
+

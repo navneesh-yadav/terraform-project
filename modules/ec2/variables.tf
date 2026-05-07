2c3bfb1f9a4e5010608ba72af9760363f37eb4fc
Ubuntu <ubuntu@ip-172-31-24-24.ec2.internal>
2026-05-07 16:23:46 +0000

    Create vpc module & VM for dev

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,26 @@
+variable "ami_id" {
+  description = "The Amazon Machine Image ID"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0" # standard Amazon Linux AMI
+}
+
+variable "instance_type" {
+  description = "The size of the server"
+  type        = string
+  default     = "t2.micro" # Free-tier eligible
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet to place the EC2 instance in"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC for the Security Group"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment name"
+  type        = string
+}
